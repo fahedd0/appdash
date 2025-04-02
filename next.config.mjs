1c1c66e79ba020fd0e7bb9e58588c3045da3ad7b
fahedd0 <fahedtammam@hotmail.com>
2025-04-02 14:26:13 +0400

    Update2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    // Redirect root to dashboard
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/dashboard',
-          permanent: true,
-        },
-      ];
-    },
-  };
+  reactStrictMode: true,
+  // Redirect root to dashboard
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
